272f54a2bf59958db7a704f77c727248d0ea43b7
Olivier Henley <olivier.henley@gmail.com>
2025-03-14 17:46:42 -0400

    wip

--- demos/src/snake_core.adb
+++ demos/src/snake_core.adb
@@ -4,13 +4,25 @@ with Ada.Real_Time; use Ada.Real_Time;
 
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 
+with Ada.Numerics.Discrete_Random;
+with Snake;
+
 package body Snake_Core is
+
+   Up : Character := 'w';
+   Down : Character := 's';
+   Left : Character := 'a';
+   Right : Character := 'd';
+   Quit : Character := 'q';
+
+   function Quit_Game (Cmd : Character) return Boolean is
+      (Cmd = Quit);
 
    protected type Data is
       procedure Set_Command (Cmd : Character);
       function Get_Command return Character;
    private
-      Command : Character := 'a';
+      Command : Character := 'w';
    end Data;
 
    protected body Data is
@@ -24,12 +36,12 @@ package body Snake_Core is
    end Data;
 
    D : Data;
-
+   
    task body Input is
       Cmd : Character;
       Avail : Boolean;
       Next_Release : Time := Clock;
-      Period : constant Time_Span := Milliseconds (100);
+      Period : constant Time_Span := Microseconds (5);
    begin
       loop
          select
@@ -39,7 +51,7 @@ package body Snake_Core is
             exit;
          else
             Get_Immediate (Cmd, Avail);
-            if Cmd /= Ascii.NUL then
+            if Cmd = Up or Cmd = Down or Cmd = Left or Cmd = Right or Cmd = Quit then
                Snake_Core.Set_Command (Cmd);
             end if;
             Next_Release := Next_Release + Period;
@@ -56,23 +68,119 @@ package body Snake_Core is
       D.Set_Command (Cmd);
    end Set_Command;
 
-   type Game_Buffer is array (1 .. 80, 1 .. 24) of Character;
+   subtype Width is Integer range 1 .. 36;
+   subtype Interior_W is Width range Width'First+1 .. Width'Last-1;
+   subtype Height is Integer range 1 .. 18;
+   subtype Interior_H is Height range Height'First+1 .. Height'Last-1;
 
-   GB : Game_Buffer;
+   type Game_Buffer is array (Height'Range, Width'Range) of Character;
 
-   function Clear_Screen return String is (ESC & "[2J" & ESC & "[H");
+   package Rand_Width is new Ada.Numerics.Discrete_Random (Interior_W);
+   Gen_Width : Rand_Width.Generator;
+
+   package Rand_Height is new Ada.Numerics.Discrete_Random (Interior_H);
+   Gen_Height : Rand_Height.Generator;
+
+   type Position is record
+      Row : Height;
+      Col : Width;
+   end record;
+   
+   function Generate_Food_Pos return Position is
+   begin
+      Rand_Width.Reset (Gen_Width);
+      Rand_Height.Reset (Gen_Height);
+      return (Rand_Height.Random (Gen_Height), Rand_Width.Random (Gen_Width));
+   end;
+
+   GB : Game_Buffer := (others => (others => ' '));
+   Food_Img : Character := '*';
+   Snake_Img : Character := 'o';
+   
+   type Snake is array (Positive range <>) of Position;
+   type Snake_Ptr is access Snake;
+
+   S : Snake_Ptr := new Snake'(1 .. 1 => Position'(Height'Last / 2, Width'Last / 2));
+
+   function Collide (S : Snake_Ptr) return Boolean is
+   begin
+      for I in S'First+1 .. S'Last loop
+         if S (S'First) = S (I) then
+            return True;
+         end if;
+      end loop;
+      if S (S'First).Col = Width'First or S (S'First).Col = Width'Last or 
+         S (S'First).Row = Height'First or S (S'First).Row = Height'Last then
+         return True;
+      end if;
+      return False;
+   end;
+
+   function Game_Over return Boolean is
+   begin
+      return Collide (S);
+   end;
+
+   --  function Snake (S : Snake_Ptr) return Snake_Ptr is
+   --     S : Snake_Ptr := new Snake (1 .. Len);
+   --  begin
+   --     for I in S'Range loop
+   --        S (I).Row := 1;
+   --        S (I).Col := I;
+   --     end loop;
+   --     return S;
+   --  end;
+
+
+   Trigger_Food : Boolean := True;
+
+   function Update_Pos (P : Position; Cmd : Character) return Position is
+      (case Cmd is when 'w' => (P.Row - 1, P.Col),
+                   when 's' => (P.Row + 1, P.Col),
+                   when 'a' => (P.Row, P.Col - 1),
+                   when 'd' => (P.Row, P.Col + 1),
+                   when others => (P.Row, P.Col));
+
+   procedure Update_Game (Cmd : Character) is
+      Pos : Position;
+   begin
+      GB (S.all (1).Row, S.all (1).Col) := ' ';
+      S.all (1) := Update_Pos (S.all (1), Cmd);
+      GB (S.all (1).Row, S.all (1).Col) := Snake_Img;
+      if Trigger_Food then
+         Pos := Generate_Food_Pos;
+         GB (Pos.Row, Pos.Col) := Food_Img;
+         Trigger_Food := False;
+      end if;
+   end;
+
+   function Trim (S : String) return String is
+            (S (S'First + 1 .. S'Last));
+
+   function Move_Cursor (R : Natural; C : Natural) return String is
+        (ESC & "[" & Trim (R'Image) & ";" & Trim (C'Image) & "H");
+
+   procedure Move_Cursor_To_End is
+   begin
+      Put (Move_Cursor (GB'Length (2), 1));
+   end Move_Cursor_To_End;
+
+   function Hide_Cursor return String is (ESC & "[?25l");
+
+
+   function Text_Color  return String is (ESC & "[38;2");
+   function Reset       return String is (ESC & "[0m");
+   function Terminator  return String is ("m"); 
 
    procedure Render_Game is
-      Clr_Rst : String := ASCII.ESC & "[2J" & ASCII.ESC & "[H";
    begin
-      GB := (others => (others => D.Get_Command));
-      for I in GB'Range (2) loop
-         for J in GB'Range (1) loop
-            Put (GB (J, I));
+      Put (Hide_Cursor);
+      for R in GB'Range (1) loop
+         for C in GB'Range (2) loop
+            Put (Move_Cursor (R+1, C));
+            Put (GB (R, C));
          end loop;
-         New_Line;
       end loop;
-      Put_Line (Clr_Rst);
    end Render_Game;
 
 end Snake_Core;
--- demos/src/snake_core.ads
+++ demos/src/snake_core.ads
@@ -9,4 +9,12 @@ package Snake_Core is
 
    procedure Render_Game;
 
+   procedure Move_Cursor_To_End;
+
+   procedure Update_Game (Cmd : Character);
+
+   function Quit_Game (Cmd : Character) return Boolean;
+   function Game_Over return Boolean;
+
+
 end Snake_Core;
