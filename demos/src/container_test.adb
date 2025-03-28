abaf4602c87981138487ce4e905a0b1e11e9ff0a
George-Ghoryafi <georgeghoryafi@outlook.com>
2025-03-28 01:12:26 -0400

    rewriting the containers to follow a tree structure. Will help later when implementing flexbox functionality

--- demos/src/container_test.adb
+++ demos/src/container_test.adb
@@ -1,137 +1,54 @@
-with Ada.Text_IO;
-with Container;
+with Ada.Text_IO; use Ada.Text_IO;
+with Container; use Container;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 
 procedure Container_Test is
-   -- Test case for a simple single-line container
-   procedure Test_Simple_Container is
-      Simple_Container : Container.Container_Type := Container.Create ("Hello, World!");
-   begin
-      Ada.Text_IO.Put_Line ("=== Simple Container Test ===");
-      Container.Render (Simple_Container);
-      Container.Free (Simple_Container);
-      Ada.Text_IO.New_Line;
-   end Test_Simple_Container;
-   
-   -- Test case for a multi-line container
-   procedure Test_Multiline_Container is
-      -- Create a multi-line string with line feeds
-      Multi_Line_Text : constant String := 
-         "This is a test" & ASCII.LF &
-         "of a multi-line" & ASCII.LF &
-         "container in our" & ASCII.LF &
-         "ASCII Engine!";
-      
-      Multi_Container : Container.Container_Type := Container.Create (Multi_Line_Text);
-   begin
-      Ada.Text_IO.Put_Line ("=== Multi-line Container Test ===");
-      Container.Render (Multi_Container);
-      Container.Free (Multi_Container);
-      Ada.Text_IO.New_Line;
-   end Test_Multiline_Container;
-   
-   -- Test case for an empty container
-   procedure Test_Empty_Container is
-      Empty_Container : Container.Container_Type := Container.Create ("");
-   begin
-      Ada.Text_IO.Put_Line ("=== Empty Container Test ===");
-      Container.Render (Empty_Container);
-      Container.Free (Empty_Container);
-      Ada.Text_IO.New_Line;
-   end Test_Empty_Container;
-   
-   -- Test case for a container with varying line lengths
-   procedure Test_Varying_Line_Lengths is
-      Varying_Text : constant String :=
-         "Short line" & ASCII.LF &
-         "This is a much longer line that should extend the container width" &
-         ASCII.LF &
-         "Medium length line here" & ASCII.LF &
-         "End";
-      
-      Varying_Container : Container.Container_Type := Container.Create (Varying_Text);
-   begin
-      Ada.Text_IO.Put_Line ("=== Varying Line Lengths Test ===");
-      Container.Render (Varying_Container);
-      Container.Free (Varying_Container);
-      Ada.Text_IO.New_Line;
-   end Test_Varying_Line_Lengths;
-   
-   -- Test case for containers with different background colors
-   procedure Test_Colored_Containers is
-      -- Red container
-      Red_Text : constant String := "This container has a red background";
-      Red_Container : Container.Container_Type := 
-         Container.Create (Red_Text, Container.Hex_To_Color ("#FF0000"));
-      
-      -- Blue container
-      Blue_Text : constant String := "This one is blue!";
-      Blue_Container : Container.Container_Type := 
-         Container.Create (Blue_Text, Container.Hex_To_Color ("#0000FF"));
-      
-      -- Green container
-      Green_Text : constant String := "And this is green" & ASCII.LF & "with multiple lines!";
-      Green_Container : Container.Container_Type := 
-         Container.Create (Green_Text, Container.Hex_To_Color ("#00FF00"));
-      
-      -- Purple container
-      Purple_Text : constant String := "Purple is a mix of red and blue";
-      Purple_Container : Container.Container_Type := 
-         Container.Create (Purple_Text, Container.Hex_To_Color ("#800080"));
-      
-      -- Orange container
-      Orange_Text : constant String := "Orange is warm and friendly";
-      Orange_Container : Container.Container_Type := 
-         Container.Create (Orange_Text, Container.Hex_To_Color ("#FFA500"));
-   begin
-      Ada.Text_IO.Put_Line ("=== Colored Containers Test ===");
-      
-      -- Test direct ANSI color code to verify terminal support
-      Ada.Text_IO.Put (ASCII.ESC & "[48;2;255;0;0m");
-      Ada.Text_IO.Put ("This text should have a red background");
-      Ada.Text_IO.Put (ASCII.ESC & "[0m");
-      Ada.Text_IO.New_Line;
-      Ada.Text_IO.New_Line;
-      
-      Ada.Text_IO.Put_Line ("Red Container:");
-      Container.Render (Red_Container);
-      Ada.Text_IO.New_Line;
-      
-      Ada.Text_IO.Put_Line ("Blue Container:");
-      Container.Render (Blue_Container);
-      Ada.Text_IO.New_Line;
-      
-      Ada.Text_IO.Put_Line ("Green Container:");
-      Container.Render (Green_Container);
-      Ada.Text_IO.New_Line;
-      
-      Ada.Text_IO.Put_Line ("Purple Container:");
-      Container.Render (Purple_Container);
-      Ada.Text_IO.New_Line;
-      
-      Ada.Text_IO.Put_Line ("Orange Container:");
-      Container.Render (Orange_Container);
-      
-      -- Free all containers
-      Container.Free (Red_Container);
-      Container.Free (Blue_Container);
-      Container.Free (Green_Container);
-      Container.Free (Purple_Container);
-      Container.Free (Orange_Container);
-      
-      Ada.Text_IO.New_Line;
-   end Test_Colored_Containers;
+   -- Create a test container
+   C1 : Container_T := Create("Hello, World!");
+   C2 : Container_T := Create("Simple Box");
+   C3 : Container_T := Create(""); -- Creating an empty container - this will be allowed to have children 
+   str : Unbounded_String;
    
 begin
-   Ada.Text_IO.Put_Line ("Container Test Suite");
-   Ada.Text_IO.Put_Line ("===================");
-   Ada.Text_IO.New_Line;
+   Put_Line("Container Test Program");
+   Put_Line("=====================");
+   New_Line;
    
-   -- Run all test cases
-   Test_Simple_Container;
-   Test_Multiline_Container;
-   Test_Empty_Container;
-   Test_Varying_Line_Lengths;
-   Test_Colored_Containers;
+   -- Display render_Rendered containers
+   Put_Line("Container 1 Rendering:");
+   Put_Line(Render(C1));
+   New_Line;
    
-   Ada.Text_IO.Put_Line ("All tests completed.");
+   Put_Line("Container 2 render_Rendering:");
+   Put_Line(Render(C2));
+   New_Line;
+   
+   -- Modify container text
+   Put_Line("Modifying container...");
+   Set_Text(C1, "Updated text");
+   New_Line;
+   
+   -- Display updated container
+   Put_Line("Container 1 after update:");
+   Put_Line(Render(C1));
+
+   -- Can grab just the text that is inside the node without the container
+   Put_Line("checking the node"); 
+   str := To_Unbounded_String(Get_Text(C1));  
+   Put_Line(To_String(str));
+   New_Line;
+
+   Put_Line("Trying out the nested contaienrs"); 
+   Container.Add_Child(C3, C1);
+   Add_Child(C3, C2);
+   Put_Line(Render(C3));
+   New_Line;
+
+
+
+
+
+   
+   Put_Line("Test completed successfully.");
 end Container_Test;
--- src/container.ads
+++ src/container.ads
@@ -1,49 +1,43 @@
-with Ada.Unchecked_Deallocation;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Containers.Vectors;
 
 package Container is
    
-   -- Color type definition
-   type Color_Type is record
-      Red   : Natural range 0..255 := 0;
-      Green : Natural range 0..255 := 0;
-      Blue  : Natural range 0..255 := 0;
+   type Color_T is (Red, Green, Blue);
+   
+   -- Container type declaration
+   type Container_T is tagged private;
+   
+   -- Constructor
+   function Create (Text : String := ""; Background: Color_T := Red) return Container_T;
+   
+   -- Accessor functions
+   function Get_Text (Self : Container_T) return String;
+
+   -- Child Management 
+   procedure Add_Child (Self : in out Container_T; Child : Container_T); 
+   --  function Get_Child (Self : Container_T; Index : Positive) return Container_T;
+   --  function Get_Number_Of_Children (Self : Container_T) return Natural;
+   
+   -- Modifier procedures
+   procedure Set_Text (Self : in out Container_T; Text : String);
+   
+   -- Rendering function
+   function Render (Self : Container_T) return String;
+   
+private
+   -- Need to clarify - Why do I have to do this?
+   type Container_Access is access Container_T;
+
+
+   package Container_Vectors is new Ada.Containers.Vectors
+     (Index_Type   => Positive,
+      Element_Type => Container_Access);
+   
+   type Container_T is tagged record
+      Text       : Unbounded_String;
+      Background : Color_T; 
+      Children   : Container_Vectors.Vector; -- Vector containing the container's Children
    end record;
-   
-   -- Default color (transparent/terminal default)
-   Default_Color : constant Color_Type := (0, 0, 0);
-   
-   -- Access type for strings
-   type String_Access is access String;
-   
-   -- Array of strings and its access type
-   type String_Array is array (Positive range <>) of String_Access;
-   type String_Array_Access is access String_Array;
-   
-   -- Container type definition
-   type Container_Type is record
-      Width      : Natural;
-      Height     : Natural;
-      Content    : String_Array_Access;
-      Background : Color_Type := Default_Color;
-   end record;
-   
-   -- Create a new container with the given text and optional background color
-   function Create (Text : String; Background_Color : Color_Type := Default_Color) 
-                   return Container_Type;
-   
-   -- Convert hex color code to Color_Type
-   function Hex_To_Color (Hex : String) return Color_Type;
-   
-   -- Set terminal background color
-   procedure Set_Background_Color (Color : Color_Type);
-   
-   -- Reset terminal colors to default
-   procedure Reset_Colors;
-   
-   -- Render the container to the terminal
-   procedure Render (Container : Container_Type);
-   
-   -- Free the memory used by the container
-   procedure Free (Container : in out Container_Type);
    
 end Container;
