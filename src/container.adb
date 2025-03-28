d777eedf53f08b92faa80f387cca70ab47d7d235
George-Ghoryafi <georgeghoryafi@outlook.com>
2025-03-28 14:35:01 -0400

    Can now add nodes as children. Started working on rendering in layers

--- src/container.adb
+++ src/container.adb
@@ -65,8 +65,19 @@ package body Container is
    procedure Add_Child (Self : in out Container_T; Child : Container_T) is
       Child_Access : constant Container_Access := new Container_T'(Child);
    begin
-      --  Self.Children.Append(Child_Access);
-      Put_Line ("Hello "); 
+      if Self.First_Child = null then 
+         Self.First_Child := Child_Access; -- Set the first child if none exists
+      else
+         -- Traverse to the last child and link the new child
+         declare
+            Current : Container_Access := Self.First_Child;
+         begin
+            while Current.Next_Sibling /= null loop
+               Current := Current.Next_Sibling;
+            end loop;
+            Current.Next_Sibling := Child_Access; -- Link the new child as the next sibling
+         end;
+      end if;
    end Add_Child;
 
    --  function Get_Child( Self : Container_T; Index : Positive ) return Container_T is
@@ -81,35 +92,76 @@ package body Container is
 
    
    
-   -- Rendering implementation
-   function Render (Self : Container_T) return String is
-      Text : constant String := To_String(Self.Text);
-      Width : constant Natural := Text'Length + 4; -- Add padding
-      Top_Bottom_Border : constant String := "+" & (Width-2) * "-" & "+";
-      Empty_Line : constant String := "|" & (Width-2) * " " & "|";
-      Text_Line : constant String := "| " & Text & " |";
-      Result : Unbounded_String;
-   begin
-      -- Build the box with the text centered
-      Append(Result, Top_Bottom_Border & ASCII.LF);
-      Append(Result, Empty_Line & ASCII.LF);
+   --  -- Rendering implementation
+   --  function Render (Self : Container_T) return String is
+   --     Text : constant String := To_String(Self.Text);
+   --     Width : constant Natural := Text'Length + 4; -- Add padding
+   --     Top_Bottom_Border : constant String := "+" & (Width-2) * "-" & "+";
+   --     Empty_Line : constant String := "|" & (Width-2) * " " & "|";
+   --     Text_Line : constant String := "| " & Text & " |";
+   --     Result : Unbounded_String;
+   --  begin
+   --     -- Build the box with the text centered
+   --     Append(Result, Top_Bottom_Border & ASCII.LF);
+   --     Append(Result, Empty_Line & ASCII.LF);
 
-      -- Conditionally render either text if it exists, or the children of the container
-      -- We need to render the last possible child first, in order to determine the size of the container
-      if Text /= "" then
-         Append(Result, Text_Line & ASCII.LF);
-      --  else
-      --     for I in reverse 1..Integer(Self.Children.Length) loop
-      --        Append(Result, Self.Children.Element(I).all.Render & ASCII.LF);
-      --     end loop;  
-      end if;    
+   --     -- Conditionally render either text if it exists, or the children of the container
+   --     -- We need to render the last possible child first, in order to determine the size of the container
+   --     if Text /= "" then
+   --        Append(Result, Text_Line & ASCII.LF);
+   --     --  else
+   --     --     for I in reverse 1..Integer(Self.Children.Length) loop
+   --     --        Append(Result, Self.Children.Element(I).all.Render & ASCII.LF);
+   --     --     end loop;  
+   --     end if;    
       
-      Append(Result, Empty_Line & ASCII.LF);
-      Append(Result, Top_Bottom_Border);
+   --     Append(Result, Empty_Line & ASCII.LF);
+   --     Append(Result, Top_Bottom_Border);
       
-      return To_String(Result);
+   --     return To_String(Result);
+   --  end Render;
+
+
+   function Render ( Self : Container_T) return String is
+   begin 
+      -- Just go through the tree, and print Container <level> as we go 
+      declare
+         Result : Unbounded_String;
+         Current : Container_Access := Self.First_Child;
+         Level : Integer := 1;
+         Text : constant String := To_String(Self.Text);
+         Width : constant Natural := Text'Length + 4; -- Add padding
+         Top_Bottom_Border : constant String := "+" & (Width-2) * "-" & "+";
+         Empty_Line : constant String := "|" & (Width-2) * " " & "|";
+         Text_Line : constant String := "| " & Text & " |";
+      begin
+         Append(Result, Top_Bottom_Border & ASCII.LF);
+         Append(Result, Empty_Line & ASCII.LF);
+
+         -- Conditionally render either text if it exists, or the children of the container
+         -- We need to render the last possible child first, in order to determine the size of the container
+         if Text /= "" then
+            Append(Result, Text_Line & ASCII.LF);
+         else
+            while Current /= null loop
+               Append(Result, Current.all.Render & ASCII.LF);
+               Current := Current.Next_Sibling;
+            end loop;  
+         end if;    
+         
+         Append(Result, Empty_Line & ASCII.LF);
+         Append(Result, Top_Bottom_Border);
+         return To_String(Result);
+      exception
+         when Constraint_Error =>
+            Put_Line("Error: Constraint violation in Render function.");
+            return "Error in rendering.";
+         when others =>
+            Put_Line("Error: Unexpected error in Render function.");
+            return "Error in rendering.";
+         
+      end;
    end Render;
-
 
 
 
